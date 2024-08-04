7cdd2ffa9c33a74878a41061c16af7b4c39e1e36
Matthias Schöffmann <matthias@dsx.at>
2024-08-04 20:56:23 +0100

    improve gh release creation

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -43,8 +43,9 @@ const main = async () => {
 	await run("pnpm changeset publish");
 	await run("git push --follow-tags");
 	const tag = (await run("git describe --abbrev=0")).replace("\n", "");
+	const name = tag.split("@").pop();
 	await run(
-		`gh release create ${tag} --title ${tag} --notes "Please refer to [CHANGELOG.md](https://github.com/mschoeffmann/astro-generic-build-filenames/blob/main/package/CHANGELOG.md) for details."`,
+		`gh release create ${tag} --title ${name} --notes "Please refer to [CHANGELOG.md](https://github.com/mschoeffmann/astro-generic-build-filenames/blob/main/package/CHANGELOG.md) for details."`,
 	);
 };
 
